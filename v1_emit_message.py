07f3bc1c1661bebd63d6e5b40f61b7732089652f
Mel <melscott2911@gmail.com>
2023-01-29 20:24:20 -0600

    Assignment 3

--- v1_emit_message.py
+++ v1_emit_message.py
@@ -1,6 +1,6 @@
 """
     This program sends a message to a queue on the RabbitMQ server.
-
+Mel Scott 1/29/2023; Emit Messages V1-RabbitMQ
     Author: Denise Case
     Date: January 14, 2023
 
@@ -16,8 +16,8 @@ ch = conn.channel()
 # use the channel to declare a queue
 ch.queue_declare(queue="hello")
 # use the channel to publish a message to the queue
-ch.basic_publish(exchange="", routing_key="hello", body="Hello World!")
+ch.basic_publish(exchange="", routing_key="hello", body="Go Chiefs!")
 # print a message to the console for the user
-print(" [x] Sent 'Hello World!'")
+print(" [x] Sent 'Go Chiefs!'")
 # close the connection to the server
 conn.close()
--- v1_listen_for_messages.py
+++ v1_listen_for_messages.py
@@ -1,8 +1,6 @@
 """
 
-Always customize this docstring. 
-
-Add your name, date, and a description of the program.
+Mel Scott 1/29/2023; Listen for Messages V1-RabbitMQ
 
 Listens for messages on the queue.
 This process runs continously. 
@@ -34,7 +32,7 @@ import pika, sys, os
 # define a main function to run the program
 def main():
     # create a blocking connection to the RabbitMQ server
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='LocalHostt'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='LocalHost'))
     # use the connection to create a communication channel
     channel = connection.channel()
     # use the channel to declare a queue
